25667cf3a8f8bb92f60ca7765fb966b17fbc526e
Juan Fco. Mange <juanfmange@gmail.com>
2024-08-28 03:08:47 -0600

    FTR - setuptools pip

--- new_project/new_project/urls.py
+++ new_project/new_project/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/posts', PostApiView.as_view()),
-    path('api/',include(router_post.urls))
+    path('api/',include(router_post.urls)),
 
    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
