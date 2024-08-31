b4084c2b17805322be271a6db684fab288588661
Juan Fco. Mange <juanfmange@gmail.com>
2024-08-31 16:11:12 -0600

    FTR - JWT

---
+++ new_project/user/api/router.py
@@ -0,0 +1,8 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path
+
+urlpatterns = [
+    path('auth/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
