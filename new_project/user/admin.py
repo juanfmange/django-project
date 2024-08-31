4303bf77811a792f2fa37b42474181bd6c46cd13
Juan Fco. Mange <juanfmange@gmail.com>
2024-08-31 15:57:44 -0600

    FTR - Usuarios y modelos de usuarios

--- new_project/user/admin.py
+++ new_project/user/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from user.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (
+            None,{
+                'fields': ('username', 'password')
+            }
+        ),
+        ( 'Informacion Personal',{'fields': ('first_name', 'last_name', 'email')}),
+        ('Otros',{'fields': ('is_active', 'is_superuser')}),
+        ('Redes Sociales',{'fields': ('web_site', 'twitter',)}),
+    )
+
--- new_project/user/models.py
+++ new_project/user/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    web_site = models.CharField(max_length=255, blank=True)
+    twitter = models.CharField(max_length=255, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
